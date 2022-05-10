f424e7b1af8f1141db73ff78b9bdc12a3e5ebb5f
valetzx <45054400+valetzx@users.noreply.github.com>
2022-05-10 10:05:43 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+sh startpg.sh
+pg_ctl stop
+pg_ctl -l ./postgresql.log start
+
+if [ ! -f "typecho" ];then
+#PG创建数据库：typecho
+create -h 127.0.0.1 -p 5432 user admin with password 'admin';
+createdb -h 127.0.0.1 -p 5432  typecho
+git clone https://github.com/typecho/typecho
+fi
+php -S 0.0.0.0:8000 -t typecho
